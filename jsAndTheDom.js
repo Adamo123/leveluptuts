b5be6ea5f9c4d82d170f99e9965a99570d4dd319
Adamo123 <adamosacchetti@outlook.com>
2019-03-16 10:37:00 -0400

    march 16

---
+++ jsAndTheDom.js
@@ -0,0 +1,12 @@
+// console.log("Hello");
+
+// I had errors because I put the script tag BEFORE my ID="app" attribute in my html
+
+document.getElementById("app").innerHTML = ' <h1> Hello Parcel </h1>
+
+
+';
+
+// console.log(test);
+
+//not sure but I think it's because parcel.js is not in the code???
