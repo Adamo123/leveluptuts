ddd4d1cc5a94b1376771a2e7b9929a7e26c8dd63
Adamo123 <adamosacchetti@outlook.com>
2019-03-17 10:43:28 -0400

    March 17

--- eloquentJsExercisesCh2.js
+++ eloquentJsExercisesCh2.js
@@ -81,72 +81,61 @@ When you have that working, modify your program to print "FizzBuzz" for numbers 
 //
 // console.log(board);
 
-let size = 8;
+// let size = 8;
+//
+// // This is smart because it will be used as a reference once inside the for loop.
+//
+// let board = "";
+//
+// // He looks at this in terms of x and y which make sense. So to build the y part (or height), he starts a for loop
+//
+// for (y = 0; y < size; y++) { // This loop loops 8 times with a \n newline at the end of every loop, but before that happens the string is built up
+//
+// for (x = 0; x < size; x++) {
+//
+// // Think of this as the grid that it is...in the first iteration, the y will always be 0...using the modulo is ingenius because it basically determines even/odd and if you look at the grid in the exercise, odd is a " " and even is a "#"
+//
+//   if ((x + y) % 2 == 0){
+//     board = board + " ";
+//   }
+//   else {
+//     board = board + "#";
+//   }
 
-// This is smart because it will be used as a reference once inside the for loop.
+// }
+// board += "\n"
+// }
+//
+// console.log(board);
+
+// Let's try this again...
+
+let size = 8;
 
 let board = "";
 
-// He looks at this in terms of x and y which make sense. So to build the y part (or height), he starts a for loop
+console.log(size);
 
 for (y = 0; y < size; y++) {
-
-// Now each line (width) has to build itself too...so...
-
-for (x = 0; x < size; x++) {
-  if ((x + y) % 2 == 0){
-    board = board + " ";
-  }
-  else {
-    board = board + "#";
+  for  (x = 0; x < size; x++) {
+      if ((x + y) % 2 == 0){
+        board += " ";
+      }
+      else {
+        board += "#";
+      }
   }
 
+  board += "\n"
 }
-board += "\n"
-}
-
 console.log(board);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//hhh
+// From the hints:
+//
+// You can build the string by starting with an empty one ("") and repeatedly adding characters. A newline character is written "\n".
+//
+// To work with two dimensions, you will need a loop inside of a loop. Put braces around the bodies of both loops to make it easy to see where they start and end. Try to properly indent these bodies. The order of the loops must follow the order in which we build up the string (line by line, left to right, top to bottom). So the outer loop handles the lines, and the inner loop handles the characters on a line.
+//
+// You’ll need two bindings to track your progress. To know whether to put a space or a hash sign at a given position, you could test whether the sum of the two counters is even (% 2).
+//
+// Terminating a line by adding a newline character must happen after the line has been built up, so do this after the inner loop but inside the outer loop.
