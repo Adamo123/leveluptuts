4ef1f5908d4fa56a4725c22b792ddde70ff4efc6
Adamo Sacchetti <adamosacchetti@Adamos-iMac.local>
2019-03-18 09:50:05 -0400

    March 18

--- eloquentJsExercisesCh3.js
+++ eloquentJsExercisesCh3.js
@@ -1,11 +1,97 @@
-console.log("Hello");
+// There are 3 ways to write functions:
 
-const power = function(base, exponent) {
-    let result = 1;
-    for (i = 0; i < exponent; i++) {
-      result *= base;
-    }
-    return result;
+// 1. Functions as values
+// 2. Declaration notation
+// 3. Arrow functions
+
+
+
+
+// console.log("Hello");
+//
+// const power = function(base, exponent) {
+//     let result = 1;
+//     for (i = 0; i < exponent; i++) {
+//       result *= base;
+//     }
+//     return result;
+// }
+//
+// console.log(power(3,2));
+
+//Now let's look at how to do this with arrow functions
+
+// const power = (base, exponent) => {
+//   let result = 1;
+//   for (i = 0; i < exponent; i++) {
+//     result *= base;
+//
+//
+//   }
+//
+// return result;
+//
+//
+//
+//
+// }
+//
+// console.log(power(2,4));
+
+// const square1 = function(x) {
+//   x = x * x;
+// }
+//
+// console.log(square1(2));
+
+// Notice how this DOESN'T return a value unless we explicitly state it like in the following
+
+// const square1 = function(x) {
+//   x = x * x;
+//   return x;
+// }
+//
+// console.log(square1(2));
+
+// But using arrow functions, it automatically returns a values...NOT TRUE!
+
+const square1 = (x) => {
+  x *= x;
+  return x;
 }
 
-console.log(power(2,2));
+console.log(square1(2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
