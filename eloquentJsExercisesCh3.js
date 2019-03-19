032d7494378edb5197e688e5182a56b91d7b73be
Adamo123 <adamosacchetti@outlook.com>
2019-03-19 11:16:46 -0400

    March 19

--- eloquentJsExercisesCh3.js
+++ eloquentJsExercisesCh3.js
@@ -35,32 +35,86 @@
 //
 //
 // }
-//
-// console.log(power(2,4));
+// recursion //********************************************************
 
-// const square1 = function(x) {
-//   x = x * x;
+// function power(base, exponent) {
+//   if (exponent == 0) {
+//     return 1;
+//   } else {
+//     return base * power(base, exponent - 1);
+//   }
 // }
 //
-// console.log(square1(2));
+// console.log(power(2, 3));
 
-// Notice how this DOESN'T return a value unless we explicitly state it like in the following
+// I don't see where it's storing it
+// Here's another example from the book
 
-// const square1 = function(x) {
-//   x = x * x;
-//   return x;
+// Consider this puzzle: by starting from the number 1 and repeatedly either adding 5 or multiplying by 3, an infinite set of numbers can be produced. How would you write a function that, given a number, tries to find a sequence of such additions and multiplications that produces that number?
+//
+// For example, the number 13 could be reached by first multiplying by 3 and then adding 5 twice, whereas the number 15 cannot be reached at all.
+//
+// Here is a recursive solution:
+
+// function findSolution(target) {
+//   function find(current, history) {
+//     if (current == target) {
+//       return history;
+//     } else if (current > target) {
+//       return null;
+//     } else {
+//       return find(current + 5, `(${history} + 5)`) ||
+//              find(current * 3, `(${history} * 3)`);
+//     }
+//   }
+//   return find(1, "1");
 // }
 //
-// console.log(square1(2));
+// console.log(findSolution(24));
 
-// But using arrow functions, it automatically returns a values...NOT TRUE!
+// Write a function min that takes two arguments and returns their minimum.
 
-const square1 = (x) => {
-  x *= x;
-  return x;
+// let findMin = function min(x,y) {
+//   if (x < y) {
+//     console.log(x);
+//   }
+//   else if (y < x) {
+//     console.log(y);
+//   }
+//   else {
+//     console.log("They are the same");
+//   }
+// }
+//
+// findMin(3, 1);
+
+// That was easy...now the harder stuff
+
+const even = function isEven(num) {
+
+  num *= -1;
+
+  if (num == 0) {
+    return true;
+  }
+  else if (num == 1) {
+    return false;
+  }
+  else {
+    return isEven (num - 2);
+
+  }
 }
 
-console.log(square1(2));
+console.log(even(-1));
+
+
+
+
+
+
+
+
 
 
 
