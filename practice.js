320697e116947502d6a524ba15f35e2959338950
Adamo123 <adamosacchetti@outlook.com>
2019-06-14 16:10:51 -0400

    June 14

--- practice.js
+++ practice.js
@@ -1,41 +1,27 @@
-// for (let num = 1 ; num <= 100; num += 1) {
-//   if (num % 3 == 0) {
-//     console.log("Fizz");
-//   }
-//   else if (num % 5 == 0) {
-//     console.log("Buzz");
-//   }
-//   else {
-//     console.log(num);
-// }
-// }
-for (let n = 1; n <= 100; n++) {
-  let output = "";
-  if (n % 3 == 0) output += "Fizz";
-  if (n % 5 == 0) output += "Buzz";
-  console.log(output || n);
-}
-// read up on empty strings and booleans and the "Short circuiting of logical operators"
+for (i = 0; i < 8; i++) {
 
-// Your code here
-for (let height = 0; height < 8; height++) {
-	let width = "#";
-  		for  (i = 0; i < 8; i++) {
-  			if (width = " ") {
-              width = "#";
+  let width;
 
-    	}
-          else {
-         width += " ";
+ if (i % 2 == 0){
+   width = " ";
+ }
 
-           }
+ else {
+   width = "#";
+ }
 
+ for (j = 0; j < 8; j++) {
 
+   if ((i + j) % 2 == 0) {
 
+     char = "#";
+     width += char;
+   }
+   else {
+    char = " ";
+    width += char;
+   }
+ }
 
-  }
-
-console.log (width);
-
-
+ console.log(width);
 }
